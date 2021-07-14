56a97b78073b8ad8a3b7a7a00a05ce661d2903b3
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-07-13 23:36:48 -0400

    setup main fragment

--- app/src/main/java/com/example/zombieshooterar/MainActivity.kt
+++ app/src/main/java/com/example/zombieshooterar/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.contentFrame, MainFragment(), "AR")
+                .addToBackStack("AR")
+                .commit()
+        }
     }
 }
