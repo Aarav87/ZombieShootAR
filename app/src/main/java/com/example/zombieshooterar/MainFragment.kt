a48f44cec2df4d519781e80a01e3000badc81cdb
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-07-14 14:56:49 -0400

    removed discovery controller

--- app/src/main/java/com/example/zombieshooterar/MainFragment.kt
+++ app/src/main/java/com/example/zombieshooterar/MainFragment.kt
@@ -3,9 +3,18 @@ package com.example.zombieshooterar
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+    lateinit var arFragment: ArFragment
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        arFragment = childFragmentManager.findFragmentById(R.id.scene_form_fragment) as ArFragment
+
+        // Hide plane discovery controller
+        arFragment.planeDiscoveryController.hide()
+        arFragment.planeDiscoveryController.setInstructionView(null)
     }
 }
