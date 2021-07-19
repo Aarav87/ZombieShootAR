32e91bac07460410912c9fc4258697223a621c42
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-07-19 16:42:33 -0400

    loaded gun model

--- app/src/main/java/com/example/zombieshooterar/MainFragment.kt
+++ app/src/main/java/com/example/zombieshooterar/MainFragment.kt
@@ -1,8 +1,12 @@
 package com.example.zombieshooterar
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 
 class MainFragment : Fragment(R.layout.fragment_main) {
@@ -10,16 +14,37 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     // Initialize ArFragment
     lateinit var arFragment: ArFragment
 
+    lateinit var gun: Renderable
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         arFragment = childFragmentManager.findFragmentById(R.id.scene_form_fragment) as ArFragment
 
         // Hide plane discovery controller
-        arFragment.planeDiscoveryController.hide()
-        arFragment.planeDiscoveryController.setInstructionView(null)
+        arFragment.instructionsController.isEnabled = false
 
         // Disable plane renderer
         arFragment.arSceneView.planeRenderer.isEnabled = false
+
+        loadModels()
+
+    }
+
+    private fun loadModels() {
+        ModelRenderable.builder()
+            .setSource(
+                context,
+                Uri.parse("https://github.com/Aarav87/ZombieShootAR/raw/master/app/sampledata/models/battle_rifle_animated/battle_rifle.glb")
+            )
+            .setIsFilamentGltf(true)
+            .build()
+            .thenAccept {
+                gun = it
+            }
+            .exceptionally {
+                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                return@exceptionally null
+            }
     }
 }
