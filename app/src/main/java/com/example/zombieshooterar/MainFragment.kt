2ee2d6656ab2874bdf18d09dbb4079cd834d97c9
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-07-22 21:12:17 -0400

    fixed lag

--- app/src/main/java/com/example/zombieshooterar/MainFragment.kt
+++ app/src/main/java/com/example/zombieshooterar/MainFragment.kt
@@ -5,16 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.ar.core.Anchor
-import com.google.ar.core.Frame
-import com.google.ar.core.Pose
-import com.google.ar.core.TrackingState
-import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 
@@ -22,14 +15,6 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     // Initialize ArFragment
     lateinit var arFragment: ArFragment
-
-    lateinit var gun: Renderable
-    lateinit var frame: Frame
-    lateinit var pose: Pose
-    lateinit var anchor: Anchor
-    lateinit var anchorNode: AnchorNode
-
-    private var placed = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,11 +25,9 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         arFragment.instructionsController.isEnabled = false
 
         // Disable plane renderer
-        arFragment.arSceneView.planeRenderer.isEnabled = false
+        arFragment.arSceneView.planeRenderer.isVisible = false
 
         loadModels()
-
-        arFragment.arSceneView.scene.addOnUpdateListener(this::onUpdateFrame)
 
     }
 
@@ -57,7 +40,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             .setIsFilamentGltf(true)
             .build()
             .thenAccept {
-                gun = it
+                attachModelToCamera(it)
             }
             .exceptionally {
                 Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
@@ -65,35 +48,21 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             }
     }
 
-    private fun onUpdateFrame(frameTime: FrameTime) {
-        frame = arFragment.arSceneView.arFrame!!
+    private fun attachModelToCamera(renderable: ModelRenderable?) {
+        val gun = TransformableNode(arFragment.transformationSystem)
 
-        if (frame.camera.trackingState == TrackingState.TRACKING) {
-            if (placed) {
-                anchor.detach()
-            }
+        // Resize model
+        gun.scaleController.minScale = 0.02f
+        gun.scaleController.maxScale = 0.03f
+        gun.localScale = Vector3(0.02f, 0.02f, 0.02f)
 
-            pose = frame.camera.pose.compose(Pose.makeTranslation(0.3f, -0.35f, -0.8f))
+        // Set model position
+        gun.localPosition = Vector3(0.3f, -0.35f, -0.8f)
 
-            anchor = arFragment.arSceneView.session!!.createAnchor(pose)
-            anchorNode = AnchorNode(anchor)
-            anchorNode.setParent(arFragment.arSceneView.scene.camera)
+        // Rotate model
+        gun.localRotation = Quaternion.axisAngle(Vector3(0f, 1f, 0f), 180f)
 
-            val gun = TransformableNode(arFragment.transformationSystem)
-
-            // Resize model
-            gun.scaleController.minScale = 0.02f
-            gun.scaleController.maxScale = 0.03f
-            gun.localScale = Vector3(0.02f, 0.02f, 0.02f)
-
-            // Rotate model
-            gun.localRotation = Quaternion.axisAngle(Vector3(0f, 1f, 0f), 180f)
-
-            gun.setParent(anchorNode)
-            gun.renderable = this.gun
-            gun.select()
-
-            placed = true
-        }
+        gun.renderable = renderable
+        arFragment.arSceneView.scene.camera.addChild(gun)
     }
 }
