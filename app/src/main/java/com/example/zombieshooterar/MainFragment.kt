f10d383dfa9c7857cea5137e481336520c34e1f4
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-07-27 00:00:01 -0400

    anchored zombie to plane

--- app/src/main/java/com/example/zombieshooterar/MainFragment.kt
+++ app/src/main/java/com/example/zombieshooterar/MainFragment.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.ar.core.Anchor
+import com.google.ar.core.Frame
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
@@ -16,6 +22,13 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     // Initialize ArFragment
     lateinit var arFragment: ArFragment
+
+    lateinit var frame: Frame
+    lateinit var planes: Collection<Plane>
+    lateinit var anchor: Anchor
+    lateinit var anchorNode: AnchorNode
+
+    private var zombieIsPlaced = false
 
     // 3D models
     lateinit var gunRenderable: ModelRenderable
@@ -64,11 +77,37 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             .build()
             .thenAccept {
                 zombieRenderable = it
+                arFragment.arSceneView.scene.addOnUpdateListener(this::onUpdate)
             }
             .exceptionally {
                 Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
                 return@exceptionally null
             }
+    }
+
+    private fun onUpdate(frameTime: FrameTime) {
+        frame = arFragment.arSceneView.arFrame!!
+        planes = frame.getUpdatedTrackables(Plane::class.java)
+
+        if (zombieIsPlaced) {
+            return
+        }
+
+        for (plane in planes) {
+            if (plane.trackingState == TrackingState.TRACKING) {
+                anchor = plane.createAnchor(plane.centerPose)
+                anchorNode = AnchorNode(anchor)
+
+                anchorNode.localScale = Vector3(0.01f, 0.01f, 0.01f)
+
+                anchorNode.setRenderable(zombieRenderable).animate(false).start()
+                arFragment.arSceneView.scene.addChild(anchorNode)
+
+                zombieIsPlaced = true
+            } else {
+                Toast.makeText(context, "Scan around to detect plane", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun attachModelToCamera() {
