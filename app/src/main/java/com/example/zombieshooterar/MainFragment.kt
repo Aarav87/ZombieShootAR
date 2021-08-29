7c2c3e508486b6da52d1bc71fd895fce8983efee
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-08-29 17:22:32 -0400

    added shooting system

--- app/src/main/java/com/example/zombieshooterar/MainFragment.kt
+++ app/src/main/java/com/example/zombieshooterar/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.example.zombieshooterar
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.Gravity
 import android.view.View
-import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.Toast
@@ -16,9 +18,11 @@ import com.google.ar.core.Plane
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.animation.ModelAnimator
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.RenderableInstance
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import java.util.*
@@ -29,15 +33,23 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     // Initialize ArFragment
     lateinit var arFragment: ArFragment
 
-    // 3D models
+    // Model Renderable
     private lateinit var gunRenderable: ModelRenderable
     private lateinit var zombieRenderable: ModelRenderable
+    private lateinit var bulletRenderable: ModelRenderable
+
+    // Renderable Instance
+    private var gunRenderableInstance: RenderableInstance? = null
 
     private var zombiesAlive = 0
     private var timeSurvived = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Get height & width of users screen
+        val height = frameLayout.measuredHeight
+        val width = frameLayout.measuredWidth
 
         arFragment = childFragmentManager.findFragmentById(R.id.scene_form_fragment) as ArFragment
 
@@ -50,6 +62,39 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         loadModels()
         addGunInfo()
 
+        shoot.setOnClickListener {
+            onShoot(height, width)
+        }
+
+    }
+
+    private fun onShoot(height: Int, width: Int) {
+        // Start gun shooting animation
+        val shootAnimation = ModelAnimator.ofAnimationTime(gunRenderableInstance, "CINEMA_4D_Main", 10F)
+        shootAnimation.start()
+
+        // Play gun shooting sound effect
+        val gunSound = MediaPlayer.create(context, R.raw.battle_riffle_sound).start()
+
+        val ray = arFragment.arSceneView.scene.camera.screenPointToRay(width / 2f, height / 2f)
+        val bullet = TransformableNode(arFragment.transformationSystem)
+        bullet.renderable = bulletRenderable
+        arFragment.arSceneView.scene.addChild(bullet)
+
+        for (i in 0..200) {
+            bullet.worldPosition = Vector3(ray.getPoint(i * 0.1f))
+
+            val nodeInContact = arFragment.arSceneView.scene.overlapTest(bullet)
+
+            if (nodeInContact != null) {
+                zombiesAlive --
+                arFragment.arSceneView.scene.removeChild(nodeInContact)
+            }
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            shootAnimation.cancel()
+        }, 100)
     }
 
     private fun addGunInfo() {
@@ -101,6 +146,23 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
                 return@exceptionally null
             }
+
+        // Render bullet model
+        ModelRenderable.builder()
+            .setSource(
+                context,
+                Uri.parse("https://github.com/Aarav87/ZombieShootAR/raw/master/app/models/bullets/battle_rifle_bullet.glb")
+            )
+            .setIsFilamentGltf(true)
+            .build()
+            .thenAccept {
+                bulletRenderable = it
+                attachModelToCamera()
+            }
+            .exceptionally {
+                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                return@exceptionally null
+            }
     }
 
     private fun zombieGenerator(frameTime: FrameTime) {
@@ -122,18 +184,18 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     }
 
     private fun attachZombieToPlane(anchor: Anchor?) {
-        val anchorNode = AnchorNode(anchor)
+        val zombie = AnchorNode(anchor)
 
         // Resize model
-        anchorNode.localScale = Vector3(0.01f, 0.01f, 0.01f)
+        zombie.localScale = Vector3(0.01f, 0.01f, 0.01f)
 
         val cameraPosition = arFragment.arSceneView.scene.camera.worldPosition
-        val anchorNodePosition = anchorNode.worldPosition
+        val anchorNodePosition = zombie.worldPosition
         val direction = Vector3.subtract(cameraPosition, anchorNodePosition)
-        anchorNode.localRotation = Quaternion.lookRotation(direction, Vector3.up())
+        zombie.localRotation = Quaternion.lookRotation(direction, Vector3.up())
 
-        anchorNode.renderable = zombieRenderable
-        arFragment.arSceneView.scene.addChild(anchorNode)
+        zombie.renderable = zombieRenderable
+        arFragment.arSceneView.scene.addChild(zombie)
 
         zombiesAlive++
     }
@@ -157,5 +219,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         gun.renderable = gunRenderable
         arFragment.arSceneView.scene.camera.addChild(gun)
+
+        gunRenderableInstance = gun.renderableInstance!!
     }
 }
